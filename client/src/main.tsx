f6366522f4d0d790cb821cdd5983db72512acd7b
ahm8811mha-sudo <ahm8811mha@gmail.com>
2026-05-28 16:29:07 +0300

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,21 +1,9 @@
-import React from "react";
+ import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "../../App";
 
-function App() {
-  return (
-    <main style={{
-      minHeight: "100vh",
-      background: "#0b0f1a",
-      color: "white",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      fontFamily: "system-ui",
-      fontSize: "32px"
-    }}>
-      Focus Flow AI
-    </main>
-  );
-}
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
